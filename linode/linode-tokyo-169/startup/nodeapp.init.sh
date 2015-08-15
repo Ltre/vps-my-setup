b6e580f350c432a2fcfa107c657edbf9767d9076
Ltre <ltrele@yeah.net>
2015-08-15 18:34:28 +0800

    disable ghost-it from linode

--- linode/linode-tokyo-169/startup/nodeapp.init.sh
+++ linode/linode-tokyo-169/startup/nodeapp.init.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 pm2 start /root/mydir/nodeapp/web-control.io/index.js --name 'web-control.io'
-NODE_ENV=production pm2 start /root/mydir/nodeapp/ghost-it/index.js --name 'ghost-it'
+# NODE_ENV=production pm2 start /root/mydir/nodeapp/ghost-it/index.js --name 'ghost-it'
 
