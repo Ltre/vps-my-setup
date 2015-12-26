b9f335e9dff78e446c0eb99270bda8c365b3f525
Ltre <ltrele@yeah.net>
2015-12-26 23:00:04 +0800

    node init

--- conoha/conoha-tokyo-49/startup/nodeapp.init.sh
+++ conoha/conoha-tokyo-49/startup/nodeapp.init.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
 NODE_ENV=production pm2 start /root/mydir/nodeapp/FakerSSH/index.js --name 'FakerSSH'
-pm2 start /root/mydir/nodeapp/web-control.io/index.js --name 'web-control.io'
+NODE_ENV=production pm2 start /root/mydir/nodeapp/web-control.io/index.js --name 'web-control.io'
 NODE_ENV=production pm2 start /root/mydir/nodeapp/ghost-it/index.js --name 'ghost-it'
 
