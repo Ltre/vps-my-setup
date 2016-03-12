e894c25aaea4a74ecb591d93052d7f9d6cf5097c
Ltre <ltrele@yeah.net>
2016-03-12 17:44:07 +0900

    a

--- conoha/conoha-tokyo-49/startup/nodeapp.init.sh
+++ conoha/conoha-tokyo-49/startup/nodeapp.init.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
 NODE_ENV=production pm2 start /root/mydir/nodeapp/FakerSSH/index.js --name 'FakerSSH'
-NODE_ENV=production pm2 start /root/mydir/nodeapp/web-control.io/index.js --name 'web-control.io'
+NODE_ENV=production pm2 start /root/mydir/nodeapp/web-control.io/io.js --name 'web-control.io'
 NODE_ENV=production pm2 start /root/mydir/nodeapp/ghost-it/index.js --name 'ghost-it'
 NODE_ENV=production pm2 start /root/mydir/nodeapp/cmd-notify/index.js --name 'cmd-notify'
