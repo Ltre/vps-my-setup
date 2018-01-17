b6bed885e7027138e283c49922f51234e8c271ea
Ltre <ltrele@yeah.net>
2018-01-17 17:28:22 +0900

    a

--- conoha/conoha-tokyo-49/startup/nohup.sh
+++ conoha/conoha-tokyo-49/startup/nohup.sh
@@ -1,2 +1,7 @@
-nohup ~/mydir/mysetup/vps-my-setup/conoha/conoha-tokyo-49/startup/nodeapp.init.sh &
-nohup ~/mydir/mysetup/vps-my-setup/conoha/conoha-tokyo-49/startup/ss.daemon.sh &
+chmod -R 777 .
+startupPath="/root/mydir/mysetup/vps-my-setup/conoha/conoha-tokyo-49/startup"
+if [ ! -d $startupPath/tmp ]; then
+    mkdir $startupPath/tmp
+fi
+nohup $startupPath/nodeapp.init.sh >$startupPath/tmp/nohup.nodeapp.out &
+nohup $startupPath/ss.daemon.sh >$startupPath/tmp/nohup.ss.daemon.out &
