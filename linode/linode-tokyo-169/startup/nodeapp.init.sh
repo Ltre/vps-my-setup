76daeeab8a44aa0029af53417d3b9e02f0655966
Ltre <ltrele@yeah.net>
2018-01-31 17:44:13 +0900

    add linode 219

--- linode/linode-tokyo-169/startup/nodeapp.init.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/sh
-pm2 start /root/mydir/nodeapp/web-control.io/index.js --name 'web-control.io'
-# NODE_ENV=production pm2 start /root/mydir/nodeapp/ghost-it/index.js --name 'ghost-it'
-
