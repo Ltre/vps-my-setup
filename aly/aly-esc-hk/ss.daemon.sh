728d042d3b0553b036f0ad2b78cca44e226c24b3
Ltre <ltrele@yeah.net>
2019-08-01 11:42:58 +0800

    alyss

---
+++ aly/aly-esc-hk/ss.daemon.sh
@@ -0,0 +1,17 @@
+while :
+do
+    ss_pids=`ps aux|awk '/[0-9] \/usr\/local\/python\/bin\/ssserver/{print $2}'`
+    ss_terminated=1
+    for teetpid in $ss_pids
+    do
+        ss_terminated=0
+    done
+    # 此处不用 ${#ss_pids[@]} == 0 判断。因永不为0，是个坑
+    if [ $teet_terminate == 1 ]; then
+        echo `date --date=now '+%Y-%m-%d %H:%M:%S %Z'`
+        echo 'wake ss..'
+        /bin/systemctl start shadowsocks.service
+    fi
+    sleep 5s
+done
+
